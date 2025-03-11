d1f709a949133c8cd6d4ff0248b475761e86a4a9
jkuya <kuyajesee@proton.me>
2025-03-11 18:02:52 +0300

    feat: enhance routing to include sign-up and error pages

--- frontend/static/js/dom.js
+++ frontend/static/js/dom.js
@@ -1,10 +1,16 @@
 import {SignInPage} from './sign-in.js'
 import { HomePage } from './homepage.js';
+import {SignUpPage} from './sign-up.js'
+import { ErrorPage } from './error.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-    if (window.location.pathname === "/") {
+    if (window.location.pathname === "/" || window.location.pathname === '/home') {
         HomePage();
-    } else if (window.location.pathname == "/sign-in") {
-        SignInPage()
+    } else if (window.location.pathname === "/sign-in") {
+        SignInPage();
+    } else if (window.location.pathname === '/sign-up') {
+        SignUpPage();
+    } else {
+        ErrorPage()
     }
 });
