957c581cc952425c32bcb61092f5cec49870cc34
jkuya <kuyajesee@proton.me>
2025-03-18 14:40:05 +0300

    refactor: update DOMContentLoaded event to fetch posts on home page load

--- frontend/static/js/dom.js
+++ frontend/static/js/dom.js
@@ -2,15 +2,19 @@ import {SignInPage} from './sign-in.js'
 import { HomePage } from './homepage.js';
 import {SignUpPage} from './sign-up.js'
 import { ErrorPage } from './error.js';
+import { getPosts } from './homepage.js';
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', async () => {
     if (window.location.pathname === "/" || window.location.pathname === '/home') {
         HomePage();
+        await getPosts()
     } else if (window.location.pathname === "/sign-in") {
         SignInPage();
     } else if (window.location.pathname === '/sign-up') {
         SignUpPage();
-    } else {
+    } else if (window.location.pathname === '/error') {
         ErrorPage()
-    }
+    } else {
+        HomePage();
+    } 
 });
