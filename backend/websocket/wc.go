e3d4be296adfef2372b8ad0b0af2844779eb1d0c
jamos <amosjoel91@gmail.com>
2025-03-18 17:18:23 +0300

    feat: add an implementation of a websocket

---
+++ backend/websocket/wc.go
@@ -0,0 +1,41 @@
+package websocket
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func WSEndpoint(w http.ResponseWriter, r *http.Request) {
+	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+
+	// upgrade to a websocket connection
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Printf("Error creating a connection: %v", err)
+	}
+
+	Read(ws)
+}
+
+func Read(conn *websocket.Conn) {
+	for {
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+			log.Printf("Error reading message: %v", err)
+			return
+		}
+		fmt.Println(p)
+
+		if err := conn.WriteMessage(messageType, p); err != nil {
+			log.Printf("Error writing message: %v", err)
+		}
+	}
+}
