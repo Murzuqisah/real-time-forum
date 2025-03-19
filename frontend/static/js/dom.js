51eabfcfb4276aab11cff4b2a216cd5c4b93319a
jkuya <kuyajesee@proton.me>
2025-03-19 18:44:04 +0300

    refactor: simplify page rendering logic by consolidating event listeners and removing unnecessary conditions

--- frontend/static/js/dom.js
+++ frontend/static/js/dom.js
@@ -1,19 +1,10 @@
-import {SignInPage} from './sign-in.js'
 import { HomePage } from './homepage.js';
-import {SignUpPage} from './sign-up.js'
-import { ErrorPage } from './error.js';
-import { getPosts } from './homepage.js';
+import { renderPage } from './homepage.js';
 
 document.addEventListener('DOMContentLoaded', async () => {
-    if (window.location.pathname === "/" || window.location.pathname === '/home') {
+    window.addEventListener("popstate", renderPage)
+    window.addEventListener("load", () => {
         HomePage();
-    } else if (window.location.pathname === "/sign-in") {
-        SignInPage();
-    } else if (window.location.pathname === '/sign-up') {
-        SignUpPage();
-    } else if (window.location.pathname === '/error') {
-        ErrorPage()
-    } else {
-        HomePage();
-    } 
+        renderPage();
+    });
 });
