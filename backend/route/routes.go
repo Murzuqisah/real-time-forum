dbc8954a1e69c95b50e5776601fe870e2ecdf77a
jkuya <kuyajesee@proton.me>
2025-03-20 12:37:07 +0300

    refactor: add authentication middleware to post and filter routes for improved security

--- backend/route/routes.go
+++ backend/route/routes.go
@@ -21,7 +21,7 @@ func InitRoutes() *http.ServeMux {
 
 	// App routes
 	r.HandleFunc("/home", middleware.Authenticate(handler.IndexHandler))
-	r.HandleFunc("/posts", (handler.HomeHandler))
+	r.HandleFunc("/posts", (middleware.Authenticate(handler.HomeHandler)))
 	r.HandleFunc("/sign-in", handler.LoginHandler)
 	r.HandleFunc("/sign-up", handler.SignupHandler)
 	r.HandleFunc("/upload", middleware.Authenticate(handler.CreatePost))
@@ -30,7 +30,7 @@ func InitRoutes() *http.ServeMux {
 	r.HandleFunc("/reaction", middleware.Authenticate(handler.ReactionHandler))
 	r.HandleFunc("/likes", middleware.Authenticate(handler.ReactionHandler))
 	r.HandleFunc("/dilikes", middleware.Authenticate(handler.ReactionHandler))
-	r.HandleFunc("/filter", handler.FilterPosts)
+	r.HandleFunc("/filter", middleware.Authenticate(handler.FilterPosts))
 
 	r.HandleFunc("/validate", handler.ValidateInputHandler)
 
