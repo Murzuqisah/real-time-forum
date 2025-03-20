58e08f94b5bbfaa8ee40b2f4619ded30e47f51cb
jkuya <kuyajesee@proton.me>
2025-03-20 19:06:52 +0300

    refactor: implement asynchronous navigation handling and improve error logging

--- frontend/static/js/dom.js
+++ frontend/static/js/dom.js
@@ -1,8 +1,29 @@
 import { renderPage } from './homepage.js';
 
 document.addEventListener('DOMContentLoaded', async () => {
-    window.addEventListener("popstate", renderPage)
-    window.addEventListener("load", () => {
-        renderPage();
-    });
+    window.addEventListener("popstate", renderPage);
+    await handleNavigation();
 });
+
+async function handleNavigation() {
+    console.log('handling navigation')
+    const path = window.location.pathname;
+
+    try {
+        const response = await fetch(path, { method: "GET" });
+        const contentType = response.headers.get("content-type");
+
+        if (contentType && contentType.includes("application/json")) {
+            const data = await response.json();
+            if (data.redirect) {
+                history.pushState({}, "", data.redirect);
+                renderPage();
+                return;
+            }
+        }
+
+        renderPage();
+    } catch (error) {
+        console.error("Error handling navigation:", error);
+    }
+}
