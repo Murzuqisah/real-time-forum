78d0b79dadc3d0ae02f047c6385eee70946ce12b
jkuya <kuyajesee@proton.me>
2025-03-22 12:18:04 +0300

    refactor: remove unused routes and authentication handlers from routes.go

--- backend/route/routes.go
+++ backend/route/routes.go
@@ -4,8 +4,6 @@ import (
 	"net/http"
 
 	"github.com/jesee-kuya/forum/backend/handler"
-	"github.com/jesee-kuya/forum/backend/middleware"
-	openauth "github.com/jesee-kuya/forum/backend/open_auth"
 )
 
 func InitRoutes() *http.ServeMux {
@@ -19,25 +17,6 @@ func InitRoutes() *http.ServeMux {
 
 	r.Handle("/", http.FileServer(http.Dir("./frontend/templates")))
 
-	// App routes
-	r.HandleFunc("/home", middleware.Authenticate(handler.IndexHandler))
-	r.HandleFunc("/sign-in", handler.LoginHandler)
-	r.HandleFunc("/sign-up", handler.SignupHandler)
-	r.HandleFunc("/upload", middleware.Authenticate(handler.CreatePost))
-	r.HandleFunc("/logout", middleware.Authenticate(handler.LogoutHandler))
-	r.HandleFunc("/comments", middleware.Authenticate(handler.CommentHandler))
-	r.HandleFunc("/reaction", middleware.Authenticate(handler.ReactionHandler))
-	r.HandleFunc("/likes", middleware.Authenticate(handler.ReactionHandler))
-	r.HandleFunc("/dilikes", middleware.Authenticate(handler.ReactionHandler))
-	r.HandleFunc("/filter", middleware.Authenticate(handler.FilterPosts))
 	r.HandleFunc("/ws", handler.HandleWebsocket)
-
-	r.HandleFunc("/validate", handler.ValidateInputHandler)
-
-	r.HandleFunc("/auth/google", openauth.GoogleAuth)
-	r.HandleFunc("/auth/google/callback", openauth.GoogleCallback)
-
-	r.HandleFunc("/auth/github", openauth.GitHubAuth)
-	r.HandleFunc("/auth/github/callback", openauth.GitHubCallback)
 	return r
 }
