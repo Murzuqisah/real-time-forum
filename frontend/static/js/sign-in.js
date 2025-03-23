db2ce2893ddf77b4cfb049439cc856edc8fc7c0a
jkuya <kuyajesee@proton.me>
2025-03-23 17:36:47 +0300

    feat: pass session token to RealTime function during user login

--- frontend/static/js/sign-in.js
+++ frontend/static/js/sign-in.js
@@ -183,7 +183,7 @@ export async function login(email, password) {
   })
   .then(data => {
     if (data.error === 'ok') {
-      RealTime(data.user)
+      RealTime(data.user, data.session)
     } else {
       alert(data.error)
     }
