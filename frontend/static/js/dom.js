aca3117324b641de64e62a66ab3a66427e737168
jkuya <kuyajesee@proton.me>
2025-03-23 17:36:56 +0300

    feat: enhance RealTime function to manage session state and user retrieval

--- frontend/static/js/dom.js
+++ frontend/static/js/dom.js
@@ -24,13 +24,27 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 });
 
-export function RealTime(user) {
+export function RealTime(user, session) {
     HomePage()
     const socket = new WebSocket(`ws://${window.location.host}/ws`);
+
+    let state = sessionStorage.getItem('pageState')
+    console.log(`state is ${state}`)
+
+    if (state === 'home') {
+        console.log('getting user')
+        waitForSocket(() => {
+            console.log('state found')
+            let session = sessionStorage.getItem('session')
+            sessionStorage.setItem('pageState', '')
+            socket.send(JSON.stringify({ type: 'getuser', session: session }))
+        });
+    }
 
     window.addEventListener('beforeunload', () => {
         let currentPage = "home"
         sessionStorage.setItem('pageState', currentPage)
+        sessionStorage.setItem('session', session)
     })
 
     socket.addEventListener('open', () => {
@@ -51,10 +65,18 @@ export function RealTime(user) {
                     console.error("Post container not found.");
                 }
                 break;
-            case 'restoreState':
-                if (data.page === 'home') {
-                    HomePage()
+            case 'error':
+                if (data.message === 'invalid session') {
+                    SignInPage();
+                    alert(message);
+                } else {
+                    alert(data.message);
                 }
+                break;
+            case 'getuser':
+                console.log('gotten user')
+                user = data.user
+                socket.send(JSON.stringify({ type: 'getposts' }))
                 break
             default:
                 console.log("Unknown message type:", data.type);
@@ -66,6 +88,14 @@ export function RealTime(user) {
         console.log("WebSocket closed. Attempting to reconnect...");
     });
 
+    const waitForSocket = (callback) => {
+        if (socket.readyState === WebSocket.OPEN) {
+            callback();
+        } else {
+            console.log('Socket not ready, retrying...');
+            setTimeout(() => waitForSocket(callback), 50);
+        }
+    };
 
 }
 
