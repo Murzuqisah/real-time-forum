2195a03dc98ac76494e41e0ae82346c14c192db1
jamos <amosjoel91@gmail.com>
2025-03-29 13:11:38 +0300

    feat: improve session validation by decoding request body and handling errors

--- backend/handler/checksession.go
+++ backend/handler/checksession.go
@@ -2,16 +2,29 @@ package handler
 
 import (
 	"encoding/json"
-	"log"
 	"net/http"
+
+	"github.com/jesee-kuya/forum/backend/repositories"
 )
 
 func CheckSession(w http.ResponseWriter, r *http.Request) {
-	mu.Lock()
-	defer mu.Unlock()
-	log.Println(SessionStore)
-	log.Println("Length of session stote is 0")
-	if len(SessionStore) == 0 {
+	var sessionData struct {
+		Session string `json:"session"`
+	}
+
+	err := json.NewDecoder(r.Body).Decode(&sessionData)
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(map[string]string{
+			"error": "Invalid request",
+		})
+		return
+	}
+
+	// Check if session exists and is valid
+	userId, err := repositories.ValidateSession(sessionData.Session)
+	if err != nil || userId == "" {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
 		json.NewEncoder(w).Encode(map[string]string{
@@ -19,7 +32,7 @@ func CheckSession(w http.ResponseWriter, r *http.Request) {
 		})
 		return
 	}
-	log.Println("Length is not zero")
+
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(map[string]string{
