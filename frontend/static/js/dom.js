3cbe1c67050d766a349c20f2d5cbb1c2570c151d
jkuya <kuyajesee@proton.me>
2025-04-06 06:24:53 +0300

    feat: add event listener for floating button to toggle create post form visibility

--- frontend/static/js/dom.js
+++ frontend/static/js/dom.js
@@ -166,8 +166,23 @@ export function RealTime(User, session) {
             });
         }
 
-
-
+        const floatingButton = document.getElementById('floatingButton');
+        if (floatingButton) {
+            floatingButton.addEventListener('click', function (e) {
+                e.preventDefault();
+                const createPostForm = document.querySelector('.create-post');
+                if (createPostForm.classList.contains('hidden')) {
+                    createPostForm.classList.remove('hidden');
+                    createPostForm.style.opacity = 1;
+                    createPostForm.style.visibility = 'visible';
+                } else {
+                    createPostForm.style.opacity = 0;
+                    createPostForm.style.visibility = 'hidden';
+                    setTimeout(() => createPostForm.classList.add('hidden'), 500); // Delay hiding with a timeout to allow opacity transition
+                }
+                console.log('clicked post add')
+            });
+        }
     };
 
     const waitForSocket = (callback) => {
