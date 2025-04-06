1ddec882ac07e746f5b2aa7b2b162e021d45372a
jkuya <kuyajesee@proton.me>
2025-04-06 07:11:57 +0300

    refactor: simplify CreatePost function by removing multipart form handling and adding base64 support for media uploads

--- backend/handler/create_post.go
+++ backend/handler/create_post.go
@@ -1,239 +1,141 @@
 package handler
 
 import (
-	"encoding/json"
+	"encoding/base64"
 	"fmt"
 	"html"
-	"io"
 	"log"
-	"mime/multipart"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/jesee-kuya/forum/backend/repositories"
+
 	"github.com/jesee-kuya/forum/backend/util"
 )
 
 /*
+
+
 UploadMedia handler function is responsible for performing server operations to enable media upload with a file size limit of up to 20 megabytes.
+
+
 */
-func CreatePost(w http.ResponseWriter, r *http.Request) {
+
+func CreatePost(filesource, title, body string, userId int) error {
 	var url string
-	if r.Method != http.MethodPost {
-		log.Println("Invalid request method:", r.Method)
-		w.WriteHeader(http.StatusInternalServerError)
-		json.NewEncoder(w).Encode(map[string]string{
-			"error": "unknown error occured. Try again later",
-		})
-		return
-	}
+
+	var file []byte
+
+	var err error
 
 	// Create the img directory if it does not exist
+
 	if err := os.MkdirAll("uploads", os.ModePerm); err != nil {
+
 		log.Println("Failed to create uploads directory:", err)
-		w.WriteHeader(http.StatusInternalServerError)
-		json.NewEncoder(w).Encode(map[string]string{
-			"error": "unknown error occured. Try again later",
-		})
-		return
+
+		return err
+
 	}
 
-	// Parse the multipart form with a 20MB limit
-	err := r.ParseMultipartForm(20 << 20)
-	if err != nil {
-		log.Println("Failed parsing multipart form:", err)
-		w.WriteHeader(http.StatusInternalServerError)
-		json.NewEncoder(w).Encode(map[string]string{
-			"error": "unknown error occured. Try again later",
-		})
-		return
-	}
+	if len(filesource) > 0 {
 
-	file, header, err := r.FormFile("uploaded-file")
-	if err != nil {
-		if err.Error() == "http: no such file" {
-			log.Println("No file uploaded, continuing process.")
-		} else {
-			log.Println("Failed retrieving media file:", err)
-			w.WriteHeader(http.StatusInternalServerError)
-			json.NewEncoder(w).Encode(map[string]string{
-				"error": "unknown error occured. Try again later",
-			})
-			return
+		file, err = base64.StdEncoding.DecodeString(filesource)
+		if err != nil {
+
+			log.Println("Failed to decode base64 string:", err)
+
+			return err
+
 		}
+
+	} else {
+		file = nil
 	}
 
 	if file != nil {
-		defer file.Close()
 
-		if header.Size > 20<<20 {
-			log.Println("File size exceeds 20MB limit")
-			w.WriteHeader(http.StatusInternalServerError)
-			json.NewEncoder(w).Encode(map[string]string{
-				"error": "The uploaded file is too large. Please upload a file less than 20MB.",
-			})
-			return
-		}
+		fileExt := http.DetectContentType(file)
 
-		// Validate MIME type and get the file extension
-		fileExt, err := ValidateMimeType(file)
-		if err != nil {
-			log.Println("Invalid extension associated with file:", err)
-			w.WriteHeader(http.StatusInternalServerError)
-			json.NewEncoder(w).Encode(map[string]string{
-				"error": "Invalid extension associated with file",
-			})
-			return
-		}
+		switch {
 
-		_, err = file.Seek(0, 0)
-		if err != nil {
-			log.Println("Failed to reset file pointer:", err)
-			w.WriteHeader(http.StatusInternalServerError)
-			json.NewEncoder(w).Encode(map[string]string{
-				"error": "unknown error occured. Try again later",
-			})
-			return
+		case strings.HasPrefix(fileExt, "image/jpeg"):
+
+			fileExt = ".jpg"
+
+		case strings.HasPrefix(fileExt, "image/png"):
+
+			fileExt = ".png"
+
+		case strings.HasPrefix(fileExt, "image/gif"):
+
+			fileExt = ".gif"
+
+		case strings.HasPrefix(fileExt, "image/webp"):
+
+			fileExt = ".webp"
+
+		case strings.HasPrefix(fileExt, "video/mp4"):
+
+			fileExt = ".mp4"
+
+		case strings.HasPrefix(fileExt, "video/quicktime"):
+
+			fileExt = ".mov"
+
+		default:
+
+			log.Println("Unsupported file type:", fileExt)
+
+			return fmt.Errorf("unsupported file type: %s", fileExt)
+
 		}
 
 		// Create a temporary file with the correct extension
+
 		tempFile, err := os.CreateTemp("uploads", "upload-*"+fileExt)
 		if err != nil {
+
 			log.Println("Failed to read file:", err)
-			w.WriteHeader(http.StatusInternalServerError)
-			json.NewEncoder(w).Encode(map[string]string{
-				"error": "unknown error occured. Try again later",
-			})
-			return
+
+			return err
+
 		}
+
 		defer tempFile.Close()
 
-		fileBytes, err := io.ReadAll(file)
-		if err != nil {
-			log.Println("Failed to read file:", err)
-			w.WriteHeader(http.StatusInternalServerError)
-			json.NewEncoder(w).Encode(map[string]string{
-				"error": "unknown error occured. Try again later",
-			})
-			return
-		}
-
 		// Write the uploaded file content to the temp file
-		_, err = tempFile.Write(fileBytes)
+
+		_, err = tempFile.Write(file)
 		if err != nil {
+
 			log.Println("Failed to write file:", err)
-			w.WriteHeader(http.StatusInternalServerError)
-			json.NewEncoder(w).Encode(map[string]string{
-				"error": "unknown error occured. Try again later",
-			})
-			return
+
+			return err
+
 		}
+
 		tempFilePath := tempFile.Name()
+
 		url = fmt.Sprintf("%v", tempFilePath)
+
 	}
 
-	cookie, err := getSessionID(r)
+	_, err = repositories.InsertRecord(util.DB, "tblPosts", []string{"post_title", "body", "media_url", "user_id"}, html.EscapeString(title), html.EscapeString(body), url, userId)
 	if err != nil {
-		log.Println("Invalid Session on cookie")
-		log.Println("cookie", cookie)
-		log.Println("SessionStore", SessionStore)
-		w.WriteHeader(http.StatusInternalServerError)
-		json.NewEncoder(w).Encode(map[string]string{
-			"error": "Invalid Session",
-		})
-		return
-	}
 
-	sessionData, ok := SessionStore[cookie]
-
-	if !ok {
-		log.Println("Invalid Session on stored sessions")
-		w.WriteHeader(http.StatusInternalServerError)
-		json.NewEncoder(w).Encode(map[string]string{
-			"error": "Invalid Session",
-		})
-		return
-	}
-
-	user, err := repositories.GetUserBySession(cookie)
-	if err != nil {
-		log.Println("Error fetching user by session:", err)
-		w.WriteHeader(http.StatusInternalServerError)
-		json.NewEncoder(w).Encode(map[string]string{
-			"error": "unknown error occured. Try again later",
-		})
-		return
-	}
-
-	id, err := repositories.InsertRecord(util.DB, "tblPosts", []string{"post_title", "body", "media_url", "user_id"}, html.EscapeString(r.FormValue("post-title")), html.EscapeString(r.FormValue("post-content")), url, sessionData)
-	if err != nil {
 		log.Println("failed to add post", err)
-		w.WriteHeader(http.StatusInternalServerError)
-		json.NewEncoder(w).Encode(map[string]string{
-			"error": "unknown error occured. Try again later",
-		})
-		return
+
+		return err
+
 	}
 
-	err = r.ParseForm()
-	if err != nil {
-		log.Println("error parsing form:", err)
-		w.WriteHeader(http.StatusInternalServerError)
-		json.NewEncoder(w).Encode(map[string]string{
-			"error": "unknown error occured. Try again later",
-		})
-		return
-	}
+	// for _, category := range categories {
 
-	categories := r.Form["category[]"]
+	// 	repositories.InsertRecord(util.DB, "tblPostCategories", []string{"post_id", "category"}, id, category)
 
-	for _, category := range categories {
-		repositories.InsertRecord(util.DB, "tblPostCategories", []string{"post_id", "category"}, id, category)
-	}
+	// }
 
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(map[string]any{
-		"error":   "ok",
-		"user":    user,
-		"session": cookie,
-	})
-}
-
-/*
-ValidateMimeType is used to check the MIME type of an uploaded file. It returns the extension associated with the file.
-*/
-func ValidateMimeType(file multipart.File) (string, error) {
-	allowedMIMEs := map[string]string{
-		"image/jpeg":    ".jpg",
-		"image/jpg":     ".jpg",
-		"image/jfif":    ".jjif",
-		"image/pjpeg":   ".pjpeg",
-		"image/pjp":     ".pjp",
-		"image/png":     ".png",
-		"image/gif":     ".gif",
-		"image/webp":    ".webp",
-		"image/svg+xml": ".svg",
-		"image/bmp":     ".bmp",
-		"image/avif":    ".avif",
-		"image/x-ico":   ".ico",
-		"image/cur":     ".cur",
-		"image/tiff":    ".tiff",
-		"image/tif":     ".tif",
-		"image/apng":    ".apng",
-	}
-
-	buffer := make([]byte, 512)
-	_, err := file.Read(buffer)
-	if err != nil {
-		log.Printf("Failed to read buffer: %v\n", err)
-		return "", fmt.Errorf("failed to read file data")
-	}
-
-	mimeType := http.DetectContentType(buffer)
-	ext, valid := allowedMIMEs[mimeType]
-	if !valid {
-		return "", fmt.Errorf("invalid file type")
-	}
-	return ext, nil
+	return nil
 }
