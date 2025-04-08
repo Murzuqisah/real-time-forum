239c20343f6a4860d141a885055566423e06ed82
jkuya <kuyajesee@proton.me>
2025-04-08 04:48:17 +0300

    refactor: modify ReactionHandler to return error instead of string response

--- backend/handler/reaction.go
+++ backend/handler/reaction.go
@@ -9,7 +9,7 @@ import (
 	"github.com/jesee-kuya/forum/backend/util"
 )
 
-func ReactionHandler(userid, postid, reactionType string) (string, error) {
+func ReactionHandler(userid, postid, reactionType string) (error) {
 	postID, _ := strconv.Atoi(postid)
 	userId, _ := strconv.Atoi(userid)
 
@@ -19,24 +19,24 @@ func ReactionHandler(userid, postid, reactionType string) (string, error) {
 		_, err := repositories.InsertRecord(util.DB, "tblReactions", []string{"user_id", "post_id", "reaction"}, userId, postID, reactionType)
 		if err != nil {
 			log.Println("Failed to insert record:", err)
-			return "", errors.New("an unexpected error occurred. try again later")
+			return errors.New("an unexpected error occurred. try again later")
 		}
-		return "+1", nil
+		return nil
 	}
 
 	if reactionType == reaction {
 		err := repositories.UpdateReactionStatus(util.DB, userId, postID)
 		if err != nil {
 			log.Println("Failed to update reaction status:", err)
-			return "", errors.New("an unexpected error occurred. try again later")
+			return errors.New("an unexpected error occurred. try again later")
 		}
-		return "-1", nil
+		return nil
 	} else {
 		err := repositories.UpdateReaction(util.DB, reactionType, userId, postID)
 		if err != nil {
 			log.Println("Failed to update reaction:", err)
-			return "", errors.New("an unexpected error occurred. try again later")
+			return errors.New("an unexpected error occurred. try again later")
 		}
 	}
-	return "+1", nil
+	return nil
 }
