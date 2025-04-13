7b085fdef3bb4f8d5b61883109b3b02bb9c05bd8
jkuya <kuyajesee@proton.me>
2025-04-13 21:29:22 +0300

    refactor: handle comments using http and not web socket

--- backend/handler/comments.go
+++ backend/handler/comments.go
@@ -1,19 +1,86 @@
 package handler
 
 import (
-	"errors"
+	"encoding/json"
 	"html"
 	"log"
+	"net/http"
+	"strconv"
 
 	"github.com/jesee-kuya/forum/backend/repositories"
 	"github.com/jesee-kuya/forum/backend/util"
 )
 
-func Comment(userId, id int, comment string) error {
-	_, err := repositories.InsertRecord(util.DB, "tblPosts", []string{"user_id", "body", "parent_id", "post_title"}, userId, html.EscapeString(comment), id, "comment")
+func Comment(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		log.Println("Invalid request method:", r.Method)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(map[string]string{
+			"error": "unknown error occured. Try again later",
+		})
+	}
+
+	cookie, err := getSessionID(r)
+	if err != nil {
+		log.Println("Invalid Session")
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(map[string]string{
+			"error": "unknown error occured. Try again later",
+		})
+		return
+	}
+
+	user, err := repositories.GetUserBySession(cookie)
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(map[string]string{
+			"error": "unknown error occured. Try again later",
+		})
+		return
+	}
+
+	comment := r.FormValue("comment")
+	itemid := r.FormValue("id")
+	id, err := strconv.Atoi(itemid)
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(map[string]string{
+			"error": "unknown error occured. Try again later",
+		})
+		return
+	}
+	pid, err := repositories.InsertRecord(util.DB, "tblPosts", []string{"user_id", "body", "parent_id", "post_title"}, user.ID, html.EscapeString(comment), id, "comment")
 	if err != nil {
 		log.Println("Failed to insert record:", err)
-		return errors.New("an unexpected error occurred. try again later")
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(map[string]string{
+			"error": "unknown error occured. Try again later",
+		})
+		return
 	}
-	return nil
+
+	cmt, err := repositories.GetPost(int(pid))
+	if err != nil {
+		log.Println("Failed to insert record:", err)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(map[string]string{
+			"error": "unknown error occured. Try again later",
+		})
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(map[string]any{
+		"error":   "ok",
+		"user":    user,
+		"session": cookie,
+		"comment": cmt,
+	})
 }
