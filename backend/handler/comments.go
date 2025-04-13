c003ed640c67595b4fb29194eef362d8e522b7a2
jkuya <kuyajesee@proton.me>
2025-04-13 21:51:23 +0300

    refactor: use the correct status message

--- backend/handler/comments.go
+++ backend/handler/comments.go
@@ -45,7 +45,7 @@ func Comment(w http.ResponseWriter, r *http.Request) {
 	comment := r.FormValue("comment")
 	if len(comment) == 0 {
 		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusInternalServerError)
+		w.WriteHeader(http.StatusBadRequest)
 		json.NewEncoder(w).Encode(map[string]string{
 			"error": "enter a comment",
 		})
