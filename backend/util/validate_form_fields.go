7dc50e9e78b65f0751083e866ae961aff4e8f3ec
jkuya <kuyajesee@proton.me>
2025-04-15 05:02:07 +0300

    refactor: add checks in validating form

--- backend/util/validate_form_fields.go
+++ backend/util/validate_form_fields.go
@@ -9,15 +9,25 @@ import (
 /*
 ValidateInput checks for the validity of input values provided via the form.
 */
-func ValidateFormFields(userName, email, password string) error {
-	if len(strings.TrimSpace(userName)) == 0 {
-		return fmt.Errorf("username field cannot be empty")
+func ValidateFormFields(userName, email, password, firstname, lastname, gender string, age int) error {
+	if len(strings.TrimSpace(userName)) == 0 || len(strings.TrimSpace(lastname)) == 0 || len(strings.TrimSpace(firstname)) == 0 {
+		return fmt.Errorf("username, lastname and firstname fields cannot be empty")
 	}
 
 	for _, v := range userName {
 		if (v < '0' || v > '9') && (v < 'a' || v > 'z') && (v < 'A' || v > 'Z') {
 			return fmt.Errorf("username must contain only letters and numbers")
 		}
+	}
+
+	for _, v := range firstname + lastname {
+		if (v < 'a' || v > 'z') && (v < 'A' || v > 'Z') {
+			return fmt.Errorf("firstname and lastname must contain only letters and numbers")
+		}
+	}
+
+	if age <= 14 {
+		return fmt.Errorf("user must be 15 years and above")
 	}
 
 	if len(strings.TrimSpace(email)) == 0 {
